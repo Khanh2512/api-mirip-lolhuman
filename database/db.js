3c1bf4ca798d07167c10e4d2d847303531a88c37
adiixyz <82200440+Adiixyz@users.noreply.github.com>
2021-12-04 23:03:41 +0800

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'adii-api.herokuapp.com';
+var url = 'https://adii-api.herokuapp.com';
 try {
-if(url == 'adii-api.herokuapp.com') throw console.log(color('Configurasi database, var url belum diisi','red'));
+if(url == 'https://adii-api.herokuapp.com') throw console.log(color('Configurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
